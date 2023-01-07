7809db4aee60abd5f06e39263336af7237dda63d
soniafarhat <sonia.farhat@esprit.tn>
2023-01-07 23:07:29 +0100

    Les fixtures avec faker

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -10,12 +10,19 @@ use Faker\Generator;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * @var Generator
+     */
+    private Generator $faker;
 
+    public function  __construct(){
+        $this->faker = Factory::create('fr_FR');
+    }
     public function load(ObjectManager $manager): void
     {
         for($i=1; $i <= 50 ; $i++) {
             $ingredient = new Ingredient();
-            $ingredient->setName('Ingrédient '. $i)
+            $ingredient->setName($this->faker->word())
                 ->setPrice(mt_rand(0,100));
             $manager->persist($ingredient);
         }
